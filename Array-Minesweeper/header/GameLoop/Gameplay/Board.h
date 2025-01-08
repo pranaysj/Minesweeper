c9e1ede52b55cb00ef16ae84679f340b7624ca87
Aryan khera <aryan.khera@outscal.com>
2025-01-08 14:46:34 +0530

    Update Board.h

--- Array-Minesweeper/header/GameLoop/Gameplay/Board.h
+++ Array-Minesweeper/header/GameLoop/Gameplay/Board.h
@@ -36,7 +36,6 @@ namespace Gameplay
 
         // Randomization
         std::default_random_engine randomEngine;
-        std::random_device randomDevice;
 
         // Private helper methods
         void initialize();
