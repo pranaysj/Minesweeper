ab0af2deb57e5b3ac70f9f37256d8c39651290c7
Aryan khera <aryan.khera@outscal.com>
2025-01-13 15:41:28 +0530

    Update GameplayManager.h

--- Array-Minesweeper/header/GameLoop/Gameplay/GameplayManager.h
+++ Array-Minesweeper/header/GameLoop/Gameplay/GameplayManager.h
@@ -18,7 +18,7 @@ namespace Gameplay
     class GameplayManager
     {
     private:
-        const float max_level_duration = 5.0f;
+        const float max_level_duration = 155.0f;
         float remaining_time;
 
         const float background_alpha = 85.f;
